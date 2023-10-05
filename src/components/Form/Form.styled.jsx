28e45d95327735bc86865172984c0284843f8cbe
PokoenkoS <pokoenkosveta@gmail.com>
2023-10-05 17:56:21 +0300

    add styles for mobile

--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -6,11 +6,11 @@ flex-direction: column;
 justify-content: space-around;
 border: 3px solid #aee2e2;
 border-radius: 30px;
-width: 250px;
+width: 200px;
 height: 200px;
 padding: 15px 30px;
 margin-top: 20px;
-@media (max-width: 768px) {
+@media screen and (min-width: 768px) {
     width: 190px;
 height: 130px;
   }
@@ -20,9 +20,9 @@ width: 100px;
 display: flex;
 flex-direction: column;
 margin-top: 8px;
-font-size: 20px;
+font-size: 12px;
 font-weight: bold;
-@media (max-width: 768px) {
+@media screen and (min-width: 768px) {
     /* width: 50px; */
     font-size: 12px;
 
@@ -35,7 +35,7 @@ border-radius: 6px;
 border-color: #aee2e2;
 width: 200px;
 height: 30px;
-@media (max-width: 768px) {
+@media screen and (min-width: 768px) {
     width: 120px;
 height: 23px;
   }
@@ -44,22 +44,23 @@ height: 23px;
 
 export const Button = styled.button`
     background-color: #aee2e2;
-    width: 150px;
+    width: 100px;
     padding: 10px;
     margin-top: 10px;
-    margin-left: 30px;
+    margin-left: 50px;
     margin-bottom: 10px;
     border-radius: 6px;
+    font-size: 10px;
     &:hover{
         background-color: red;
         color:#aee2e2;
     }
-    @media (max-width: 768px) {
-    width: 100px;
+    @media screen and  (min-width: 768px) {
+    width: 150px;
     /* height: px; */
     font-size: 12px;
     padding: 5px;
-    margin-left: 0px;
+    margin-left: 30px;
   }
     
 `
